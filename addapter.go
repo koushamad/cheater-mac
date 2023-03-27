8e6427aa157ba85421923c1486dbf5b1484e41af
kousha ghodsizad <kousha.ghodsizad@gmail.com>
2023-03-27 14:23:48 +0200

    change connection to ws

--- addapter.go
+++ addapter.go
@@ -2,121 +2,63 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/websocket"
-	"log"
-	"net/url"
-	"os"
-	"os/signal"
-	"time"
 )
 
-const Client = "mac"
-const ApiKey = "test"
-
-var conn *websocket.Conn
-var done chan struct{}
-var dis chan struct{}
-
-func init() {
-	dis = make(chan struct{})
-	done = make(chan struct{})
-}
-
 type Message struct {
-	ApiKey  string `json:"apiKey"`
+	APIKey  string `json:"apiKey"`
 	Client  string `json:"client"`
 	Content string `json:"content"`
 }
 
-func ListenWS(meg chan Message) {
-	connect()
-	listen(meg)
-	register()
-	disconnect()
-	<-done
-	time.Sleep(1 * time.Second)
-	conn.Close()
-	ListenWS(meg)
-}
+const Client = "mac"
 
-func connect() {
+var apiKey = "test"
 
-	// Define the WebSocket URL
-	serverURL := url.URL{Scheme: "wss", Host: "cheater-server-mbmu9.ondigitalocean.app", Path: "/ws"}
+//var url = "ws://localhost:8080/ws"
+var url = "wss://cheater-server-mbmu9.ondigitalocean.app/ws"
 
+func Listen(req chan Message, res chan Message) {
 	// Connect to the WebSocket server
-	connection, _, err := websocket.DefaultDialer.Dial(serverURL.String(), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
 	if err != nil {
-		log.Fatal("Error connecting to the WebSocket server:", err)
-		time.Sleep(5 * time.Second)
-		connect()
+		showErrorDialog(err)
+	}
+	defer conn.Close()
+
+	// Send a message to register the client
+	message := Message{APIKey: apiKey, Client: Client, Content: "register"}
+	if err := conn.WriteJSON(message); err != nil {
+		showErrorDialog(err)
 	}
 
-	conn = connection
-}
-
-func disconnect() {
-	// Handle termination signals
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	select {
-	case <-interrupt:
-		log.Println("Received interrupt signal, closing connection...")
-		err := conn.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-		if err != nil {
-			log.Println("Error sending close message:", err)
-		}
-	}
-	<-dis
-	<-done
-}
-
-func listen(meg chan Message) {
-	// Listen for server messages and print them in the console
 	go func() {
-		defer close(done)
 		for {
-			_, data, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("Error reading server message:", err)
-				continue
+			select {
+			case msg := <-res:
+				if err := conn.WriteJSON(msg); err != nil {
+					showErrorDialog(err)
+				}
 			}
-			message := Message{}
-			err = json.Unmarshal(data, &message)
-			if err != nil {
-				log.Println("Error unmarshalling JSON:", err)
-				continue
-			}
-			meg <- message
 		}
 	}()
-}
 
-func register() {
-	// Send a message with the JSON content
-	message := Message{
-		ApiKey:  ApiKey,
-		Client:  Client,
-		Content: "register",
-	}
+	// Wait for incoming messages
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			showErrorDialog(err)
+		}
 
-	SendMessage(message)
-}
+		// Decode the incoming message
+		var message Message
+		if err := json.Unmarshal(msg, &message); err != nil {
+			showErrorDialog(err)
+			continue
+		}
 
-func SendMessage(message Message) {
-	data, err := json.Marshal(message)
-	if err != nil {
-		log.Println("Error marshalling JSON:", err)
-		return
-	}
-
-	if err := conn.WriteMessage(websocket.TextMessage, data); err != nil {
-		log.Println("Error sending message:", err)
-		<-dis
-		<-done
-
-		time.Sleep(5 * time.Second)
-		SendMessage(message)
+		fmt.Println("Received: ", message)
+		req <- message
 	}
 }
--- main.go
+++ main.go
@@ -1,85 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
-	"github.com/0x9ef/openai-go"
 	"github.com/atotto/clipboard"
-	"golang.org/x/net/context"
-	"sync"
 )
 
-type textData struct {
-	Text string `json:"text"`
-}
-
 var (
-	lock = sync.Mutex{}
+	req chan Message
+	res chan Message
 
-	cheatContent              = make(chan Message, 1)
-	aiAns                     = make(chan string, 1)
-	aiTranslate               = make(chan string, 1)
-	cheatContentText          = ""
-	aiAnsText                 = ""
-	aiTranslateText           = ""
-	programmingLanguages      = []string{"Go", "Python", "JavaScript", "Java", "C++", "C#", "Ruby", "PHP", "Swift", "Rust", "Scala", "Kotlin", "Dart", "Objective-C", "Perl", "Bash Script"}
-	languages                 = []string{"Level A1", "Level A2", "Level B1", "Level B2", "Level C1", "Level C2"}
-	defaultLanguage           = "Level B1"
-	defaultProgramingLanguage = "Go"
-	defaultApiToken           = "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"
+	cheatContent = make(chan Message, 1)
+	contentText  = ""
 
-	myApp                    = app.New()
-	myWindow                 = myApp.NewWindow("Smart Cheater AI")
-	label1                   = widget.NewLabelWithStyle(cheatContentText, fyne.TextAlignLeading, fyne.TextStyle{Monospace: true})
-	label2                   = widget.NewLabelWithStyle(aiAnsText, fyne.TextAlignLeading, fyne.TextStyle{Monospace: true})
-	scroll1                  = container.NewScroll(label1)
-	scroll2                  = container.NewScroll(label2)
-	apiTokenInput            = widget.NewEntry()
-	maxTokensInput           = widget.NewEntry()
-	maxTokens                = 1000
-	loadingIndicator         = widget.NewProgressBarInfinite()
-	selectProgramingLanguage = widget.NewSelect(programmingLanguages, func(selected string) { defaultProgramingLanguage = selected })
-	selectLanguage           = widget.NewSelect(languages, func(selected string) { defaultLanguage = selected })
-	askButton                = widget.NewButton("Answer", onAskAITapped)
-	translateButton          = widget.NewButton("Translate", onTranslateAITapped)
-	copyButton               = widget.NewButton("Copy", copyText)
-	promptInput              = widget.NewEntry()
-	configInputs             = container.NewHBox(selectProgramingLanguage, selectLanguage)
-	apiInput                 = container.NewHSplit(apiTokenInput, maxTokensInput)
-	header                   = container.NewHSplit(configInputs, apiInput)
-	split                    = container.NewHSplit(scroll1, scroll2)
-	body                     = container.NewVSplit(header, split)
-	buttons                  = container.NewHBox(askButton, translateButton, copyButton)
-	footer                   = container.NewHSplit(promptInput, buttons)
-	content                  = container.NewVSplit(body, footer)
+	myApp            = app.New()
+	myWindow         = myApp.NewWindow("Smart Cheater AI")
+	label            = widget.NewLabelWithStyle(contentText, fyne.TextAlignLeading, fyne.TextStyle{Monospace: true})
+	body             = container.NewScroll(label)
+	loadingIndicator = widget.NewProgressBarInfinite()
+	askButton        = widget.NewButton("Answer", onAnswerTapped)
+	copyButton       = widget.NewButton("Copy", copyText)
+	promptInput      = widget.NewEntry()
+	buttons          = container.NewHBox(askButton, copyButton)
+	footer           = container.NewHSplit(promptInput, buttons)
+	content          = container.NewVSplit(body, footer)
 )
 
 func init() {
+	req = make(chan Message)
+	res = make(chan Message)
+
 	myWindow.Resize(fyne.NewSize(800, 600))
-	apiTokenInput.SetPlaceHolder("Enter Chat GPT API Token")
-	label1.Wrapping = fyne.TextWrapWord
-	label2.Wrapping = fyne.TextWrapWord
-	label1.Resize(fyne.NewSize(300, label1.MinSize().Height))
-	label2.Resize(fyne.NewSize(300, label2.MinSize().Height))
-	apiTokenInput.SetText(defaultApiToken)
-	maxTokensInput.SetText(fmt.Sprintf("%d", maxTokens))
+	label.Wrapping = fyne.TextWrapWord
+	label.Resize(fyne.NewSize(300, label.MinSize().Height))
 	promptInput.SetPlaceHolder("Enter Your Prompt")
 	promptInput.SetText("Solve this challenge")
-	selectProgramingLanguage.SetSelected(defaultProgramingLanguage)
-	selectLanguage.SetSelected(defaultLanguage)
 	footer.SetOffset(0.99)
-	apiInput.SetOffset(0.9)
-	header.SetOffset(0.01)
-	body.SetOffset(0.01)
 	content.SetOffset(0.99)
 	myWindow.SetContent(content)
 	buttons.Add(loadingIndicator)
 	loadingIndicator.Hide()
-	lock.Lock()
 }
 
 func main() {
@@ -93,23 +56,21 @@ func listenServer() {
 	for {
 		select {
 		case msg := <-cheatContent:
-			cheatContentText += msg.Content +
-				"\n\n" + "-----------------------------------------------------------------------------\n\n"
-			label1.SetText(cheatContentText)
-			lock.Unlock()
-		case text := <-aiAns:
-			aiAnsText = text
-			label2.SetText(aiAnsText)
-		case text := <-aiTranslate:
-			aiTranslateText = text
-			label1.SetText(aiTranslateText)
+			contentText = msg.Content + "\n\n" + "--------------------------------------------------------------------------------------------------\n\n"
+			label.SetText(label.Text + contentText)
 		}
 	}
 }
 
-func updateAttributes() {
-	defaultApiToken = apiTokenInput.Text
-	maxTokens, _ = fmt.Sscanf(maxTokensInput.Text, "%d", &maxTokens)
+func runServer() {
+	go Listen(req, res)
+
+	for {
+		select {
+		case m := <-req:
+			cheatContent <- m
+		}
+	}
 }
 
 func showErrorDialog(err error) {
@@ -117,78 +78,23 @@ func showErrorDialog(err error) {
 }
 
 func copyText() {
-	prompt := promptInput.Text + " in " + defaultProgramingLanguage + ":\n\n" + cheatContentText
-	err := clipboard.WriteAll(prompt)
+	err := clipboard.WriteAll(contentText)
 	if err != nil {
-		fmt.Println("Error setting clipboard:", err)
+		showErrorDialog(err)
 		return
 	}
 }
 
-func onAskAITapped() {
-	updateAttributes()
-	go func() {
-		if lock.TryLock() {
-			loadingIndicator.Show()
-			prompt := promptInput.Text + " in " + defaultProgramingLanguage + " programing language and explain solution in the " + defaultLanguage + "\n\n" + cheatContentText
+func onAnswerTapped() {
+	loadingIndicator.Show()
 
-			e := openai.New(defaultApiToken)
-			r, err := e.Completion(context.Background(), &openai.CompletionOptions{
-				Model:       openai.ModelGPT3TextDavinci003,
-				MaxTokens:   maxTokens,
-				Prompt:      []string{prompt},
-				Temperature: 0.3,
-				N:           1,
-			})
+	prompt := promptInput.Text + "\n\n" + contentText
 
-			if err != nil {
-				showErrorDialog(err)
-			} else {
-				aiAns <- r.Choices[0].Text
-			}
-			loadingIndicator.Hide()
-			lock.Unlock()
-		}
-	}()
-}
-
-func onTranslateAITapped() {
-	updateAttributes()
-	go func() {
-		if lock.TryLock() {
-			loadingIndicator.Show()
-			prompt := "Translate this text to " + defaultLanguage + "\n\n" + cheatContentText
-
-			e := openai.New(defaultApiToken)
-			r, err := e.Completion(context.Background(), &openai.CompletionOptions{
-				Model:       openai.ModelGPT3TextDavinci003,
-				MaxTokens:   maxTokens,
-				Prompt:      []string{prompt},
-				Temperature: 0.3,
-				N:           1,
-			})
-
-			if err != nil {
-				showErrorDialog(err)
-			} else {
-				aiTranslate <- r.Choices[0].Text
-			}
-
-			loadingIndicator.Hide()
-			lock.Unlock()
-		}
-	}()
-}
-
-func runServer() {
-
-	msg := make(chan Message)
-	go ListenWS(msg)
-
-	for {
-		select {
-		case m := <-msg:
-			cheatContent <- m
-		}
+	res <- Message{
+		APIKey:  apiKey,
+		Client:  "ios",
+		Content: prompt,
 	}
+
+	loadingIndicator.Hide()
 }
